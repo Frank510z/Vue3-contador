1714a938cdaf751e86502d0745e3acffedf79605
Frank510z <francis.fjro@gmail.com>
2024-07-02 11:47:12 -0600

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<!-- Template - HTML-->
+<template>
+  <div class="contenedor">
+    <h1>{{ mensaje }}</h1>
+    <img src="./assets/logo.svg" alt="" height="200px" />
+
+    <hr />
+
+    <h2>Contador: {{ contador }}</h2>
+    <button @click="inclementar()">+1</button>
+    <button @click="disminuir()">-1</button>
+    <button @click="reset()">Reset</button>
+  </div>
+</template>
+
+<!-- script - JavaScript-->
+<script>
+export default {
+  data: () => ({
+    contador: 0,
+    mensaje: "Hola Mundo",
+  }),
+
+  methods: {
+    inclementar() {
+      this.contador++;
+    },
+
+    disminuir() {
+      this.contador--;
+    },
+
+    reset() {
+      this.contador = 0;
+    },
+
+  },
+};
+</script>
+
+<!-- Style - CSS-->
+<style scoped>
+h1 {
+  color: red;
+  font-size: 3rem;
+}
+
+.contenedor {
+  text-align: center;
+}
+
+h2 {
+  font-size: 2rem;
+}
+
+button {
+  padding: 17px 40px;
+  border-radius: 10px;
+  border: 0;
+  margin: 0 6px;
+  background-color: #32475c;
+  letter-spacing: 1.5px;
+  font-size: 15px;
+  transition: all 0.3s ease;
+  box-shadow: #40b27f 0px 10px 0px 0px;
+  color: hsl(0, 0%, 100%);
+  cursor: pointer;
+}
+
+button:hover {
+  box-shadow: #ffe600 0px 7px 0px 0px;
+}
+
+button:active {
+  transform: translateY(5px);
+  transition: 200ms;
+  color: #ffe600;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/base.css' // Importamos el css
+
+import { createApp } from 'vue' // se crea una estancia createApp donde contendra todo
+import App from './App.vue' 
+
+createApp(App).mount('#app') //
